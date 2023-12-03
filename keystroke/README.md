00a1a636ad0fea1d9ee7b43205554a1ed4841f09
Jacob Baber <jacob.baber@ou.edu>
2023-12-03 15:57:22 -0600

    APIv2 working and is more accurate :D

--- keystroke/README.md
+++ keystroke/README.md
@@ -3,3 +3,11 @@ Run: python -m PyInstaller -y keystroke.py to from this directory to build the e
 ## runs the api
 
 python -m uvicorn IFApi:app --reload
+
+On the same data Jacob and Jacob2: 89.92 Normal Score
+
+
+## runs apiv2
+python -m uvicorn IFApiV2:app --reload
+
+On the same data Jacob and Jacob2: 91.36 Normal Score
--- keystroke/keystrokev2.py
+++ keystroke/keystrokev2.py
@@ -4,36 +4,41 @@ import csv
 from pynput import keyboard
 
 # Function to handle keypress events
-global current_time_ms
 global delta_time_ms
-global release_time_ms
-global press_time_ms
 global duration_ms
-global key_char
+global prev_time_ms
+
+
 
 # Initialize variables
-current_time_ms = 0
+
 delta_time_ms = 0
-release_time_ms = 0
-press_time_ms = 0
 duration_ms = 0
-key_char = ""
+prev_time_ms = 0
+keys_being_pressed = {}
 
 def on_key_press(key):
-    global current_time_ms
     global delta_time_ms
-    global key_char
+    global keys_being_pressed
+    global prev_time_ms
 
     try:
+        if key == keyboard.Key.shift:
+            return 
+        if key == keyboard.Key.esc:
+            return False
+        
         current_time_ms = time.time() * 1000  # Convert current time to milliseconds
-        key_char = key.char  # Get the character of the pressed key
-        delta_time_ms = current_time_ms - on_key_press.prev_time_ms
+        
+        keys_being_pressed[key] = current_time_ms  # Add the key to the dictionary
+        delta_time_ms = current_time_ms - prev_time_ms
 
         # If more than 2 seconds have passed, reset the time
-        if delta_time_ms > 2000:
+        if delta_time_ms > 5000:
             delta_time_ms = 0
 
-        on_key_press.prev_time_ms = current_time_ms
+        prev_time_ms = current_time_ms
+
 
     except AttributeError:
         # Handle special keys that don't have a 'char' attribute
@@ -41,28 +46,34 @@ def on_key_press(key):
 
 
 def on_key_release(key):
-    global release_time_ms
+    if key == keyboard.Key.shift:
+        return
+    
     global duration_ms
-    global key_char
+    global keys_being_pressed
+    global delta_time_ms
+    global prev_time_ms
 
-    release_time_ms = time.time() * 1000  # Convert current time to milliseconds
+    if key in keys_being_pressed:
+        press_time_ms = keys_being_pressed[key]
+        release_time_ms = time.time() * 1000  # Convert current time to milliseconds
+        duration_ms = release_time_ms - press_time_ms
+        del keys_being_pressed[key]  # Remove the key from the dictionary
+        print(f"Key: {key}, Time Since Previous Key (ms): {delta_time_ms:.2f}ms, Duration (ms): {duration_ms:.2f}ms")
 
-    # Calculate the duration of the keypress
-    duration_ms = release_time_ms - current_time_ms
+        # Write the keypress information to a CSV file
+        with open(f'{user_name}_keypress_data.csv', 'a', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([key, delta_time_ms, duration_ms])
 
-    # Print the keypress information
-    print(f"Key: {key_char}, Time Since Previous Key (ms): {delta_time_ms:.2f}ms, Duration (ms): {duration_ms:.2f}ms")
 
-    # Write the keypress information to a CSV file
-    with open('keypress_data.csv', 'a', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow([key_char, delta_time_ms, duration_ms])
+
 
 # Get the name of the user
 user_name = input("Enter your name: ")
 
 # Initialize the previous keypress time to the current time
-on_key_press.prev_time_ms = time.time() * 1000
+prev_time_ms = time.time() * 1000
 
 # Create a listener for keyboard events
 with keyboard.Listener(on_press=on_key_press, on_release=on_key_release) as listener:
